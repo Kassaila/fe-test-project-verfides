6eba418709322a47c6817e8c9900ccf2a5ec9b85
dsymonov <dsymonov@justcoded.co>
2019-10-03 12:39:20 +0300

    Slider state fix

--- src/js/modules/services-slider.js
+++ src/js/modules/services-slider.js
@@ -3,37 +3,49 @@ import Swiper from 'swiper';
 const servicesSlider = () => {
   const init = () => {
     const $win = $(window);
+    let slider;
+    let state = 'destroy';
 
-    const slider = new Swiper('.swiper-container', {
-      // Optional parameters
-      init: false,
-      direction: 'horizontal',
-      speed: 300,
-      loop: false,
-      autoHeight: true,
-      navigation: {
-        nextEl: '.swiper-btn_next',
-        prevEl: '.swiper-btn_prev',
-      },
-      breakpoints: {
-        480: {
-          slidesPerView: 2,
-          spaceBetween: 10,
+    const _sliderInit = () => {
+      slider = new Swiper('.swiper-container', {
+        // Optional parameters
+        init: false,
+        direction: 'horizontal',
+        speed: 300,
+        loop: false,
+        autoHeight: true,
+        navigation: {
+          nextEl: '.swiper-btn_next',
+          prevEl: '.swiper-btn_prev',
         },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 10,
+        breakpoints: {
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          },
         },
-      },
-    });
+      });
 
-    if ($win.width() > 480) slider.init();
+      slider.init();
+      state = 'init';
+    };
+
+    const _sliderDestroy = () => {
+      slider.destroy();
+      state = 'destroy';
+    };
+
+    if ($win.width() > 480) _sliderInit();
 
     $win.on('resize', () => {
-      if ($win.width() <= 480) {
-        slider.destroy();
-      } else {
-        slider.init();
+      if ($win.width() <= 480 && state !== 'destroy') {
+        _sliderDestroy();
+      } else if ($win.width() > 480 && state === 'destroy') {
+        _sliderInit();
       }
     });
   };
